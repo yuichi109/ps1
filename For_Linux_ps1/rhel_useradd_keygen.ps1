24e0da854b1112b952e8bd1b6a650c9a57e3adb0
Yuichi Matsuo <yuichi.matsuo.kf@cocoo.co.jp>
2023-10-17 13:35:27 +0900

    Windows用Powershell Linux useradd keygen

---
+++ For_Linux_ps1/rhel_useradd_keygen.ps1
@@ -0,0 +1,16 @@
+#For RHEL Useradd sshkey gen
+#$USERと $TGTIPを対象に合わせてくください
+$USER="username"    #changeme
+$TGTIP="10.10.10.10"    #changeme
+$KEYPATH=[Environment]::GetFolderPath('MyDocuments') + "\KEY-GEN"
+$TGT="/home/${USER}/.ssh"
+$PUBKEY="$KEYPATH\$USER.pub"
+
+mkdir $KEYPATH -force
+ssh-keygen -N '""' -m PEM -f $KEYPATH\$USER
+cat $PUBKEY | ssh user@${TGTIP} "sudo -S useradd ${USER};\
+sudo -S mkdir -p ${TGT};\
+sudo -S chown ${USER}:${USER} ${TGT};\
+sudo -S tee -a ${TGT}/authorized_keys;\
+sudo -S chown ${USER}:${USER} ${TGT}/authorized_keys;\
+sudo -S chmod 600 ${TGT}/authorized_keys"
